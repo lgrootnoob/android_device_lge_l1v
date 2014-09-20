95b3b1d93c927b9f543b582ae41e6a9f5ed15a34
lgrootnoob <jaclane299@gmail.com>
2014-09-20 16:17:19 -0500

    dont be stupid, we have local path

--- device_l1v.mk
+++ device_l1v.mk
@@ -1,16 +1,14 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 # The gps config appropriate for this device
 $(call inherit-product, device/common/gps/gps_us_supl.mk)
-
+LOCAL_PATH := device/lge/l1v
 PRODUCT_COPY_FILES += \
 	$(LOCAL_PATH)/recovery/postrecoveryboot.sh:recovery/root/sbin/postrecoveryboot.sh \
-	$(LOCAL_PATH)/../fstab.l1v:root/fstab.l1v
-	$(LOCAL_PATH)/../fstab.l1v:root/recovery.fstab
+	$(LOCAL_PATH)/fstab.l1v:root/fstab.l1v
+	$(LOCAL_PATH)/fstab.l1v:root/recovery.fstab
 
 $(call inherit-product-if-exists, vendor/lge/l1v/l1v-vendor.mk)
-$(LOCAL_PATH)/recovery/fstab.l1v:recovery/root/fstab.l1v
 DEVICE_PACKAGE_OVERLAYS += device/lge/l1v/overlay
-LOCAL_PATH := device/lge/l1v
 
 $(call inherit-product, build/target/product/full.mk)
 
