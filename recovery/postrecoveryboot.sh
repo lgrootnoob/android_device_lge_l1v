539f025f96a6e0f17d9ecea5cd9656f34ee4de31
lgrootnoob <lgrootnoob@github.com>
2014-09-21 01:51:57 -0500

    Tweak postrecoveryboot and tweak fstab

--- recovery/postrecoveryboot.sh
+++ recovery/postrecoveryboot.sh
@@ -1,18 +1,10 @@
 #!/sbin/sh
-
-# wait for system to settle
-# sleep 5
-# echo "interactive" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
+echo "interactive" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
 # change the LED pattern to something less disco
-# echo 2 > /sys/devices/i2c-0/0-0032/led_pattern
+echo 2 > /sys/devices/i2c-0/0-0032/led_pattern
 # busybox dd if=/dev/zero of=/dev/block/platform/msm_sdcc.1/by-name/misc conv=notrunc bs=1 count=32 seek=64
-# echo yo dawg, i heard you like scripts. so......
-## Fix wtfe LG has aboot doing on reboot recovery
-#mke2fs -t ext4 /dev/block/platform/msm_sdcc.1/by-name/userdata
-
-# Zero out boot recovery and wipe data command
-#dd if=/dev/zero of=/dev/block/platform/msm_sdcc.1/by-name/misc seek=64 count=20 bs=1
-
+# Fix wtfe LG has aboot doing on reboot recovery
+mke2fs -t ext4 /dev/block/platform/msm_sdcc.1/by-name/userdata
 # wait for system to settle
 #sleep 5
 
