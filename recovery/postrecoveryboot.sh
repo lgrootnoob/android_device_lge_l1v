e86f80f47e02ab04baca6678252df3aa1c604408
lgrootnoob <lgrootnoob@github.com>
2014-09-22 11:55:28 -0500

    deneuter sim tested and works now

--- recovery/postrecoveryboot.sh
+++ recovery/postrecoveryboot.sh
@@ -1,10 +1,9 @@
 #!/sbin/sh
 umount /system
-busybox mount /system
-busybox mount -o remount,rw /system
+busybox mount -o rw /dev/block/mmcblk0p14 /system
 echo "interactive" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
 # Fix wtfe LG has aboot doing on reboot recovery
-mke2fs -t ext4 /dev/block/platform/msm_sdcc.1/by-name/userdata
+#mke2fs -t ext4 /dev/block/platform/msm_sdcc.1/by-name/userdata
 busybox dd if=/dev/zero of=/dev/block/platform/msm_sdcc.1/by-name/misc conv=notrunc bs=1 count=32 seek=64
 # Tell us that it worked!
 rm -rf /system/app/CloudSetupWizard.apk
