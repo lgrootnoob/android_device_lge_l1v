df2e4e2160d8f46936e8c7d4098c7c88f77b0735
lgrootnoob <lgrootnoob@github.com>
2014-09-22 10:29:42 -0500

    Really fixed problems, testing now

--- recovery/postrecoveryboot.sh
+++ recovery/postrecoveryboot.sh
@@ -1,15 +1,12 @@
 #!/sbin/sh
-busybox mount /cache
+umount /system
 busybox mount /system
+busybox mount -o remount,rw /system
 echo "interactive" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
-# change the LED pattern to something less disco
-echo 2 > /sys/devices/i2c-0/0-0032/led_pattern
-# busybox dd if=/dev/zero of=/dev/block/platform/msm_sdcc.1/by-name/misc conv=notrunc bs=1 count=32 seek=64
 # Fix wtfe LG has aboot doing on reboot recovery
 mke2fs -t ext4 /dev/block/platform/msm_sdcc.1/by-name/userdata
 busybox dd if=/dev/zero of=/dev/block/platform/msm_sdcc.1/by-name/misc conv=notrunc bs=1 count=32 seek=64
 # Tell us that it worked!
-rm /system/app/CloudSetupWizard.apk
-rm /system/app/ChargingWallpaperPickerbin.apk
-rm /system/app/LGVZWSetupWizard_JB.apk
-echo $(mount) >> /cache/recovery/log
+rm -rf /system/app/CloudSetupWizard.apk
+rm -rf /system/app/ChargingWallpaperPickerbin.apk
+rm -rf /system/app/LGVZWSetupWizard_JB.apk
