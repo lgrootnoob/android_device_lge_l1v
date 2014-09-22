b60c7870302152c465f1a85b520268a535652b39
lgrootnoob <lgrootnoob@github.com>
2014-09-22 15:33:16 -0500

    move /data mount to img file on sdcard

--- recovery/postrecoveryboot.sh
+++ recovery/postrecoveryboot.sh
@@ -1,11 +1,18 @@
 #!/sbin/sh
 umount /system
+umount /external_sd
 busybox mount -o rw /dev/block/mmcblk0p14 /system
+busybox mount -o rw /dev/block/platform/msm_sdcc.3/by-num/p1 /external_sd
 echo "interactive" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
-# Fix wtfe LG has aboot doing on reboot recovery
 #mke2fs -t ext4 /dev/block/platform/msm_sdcc.1/by-name/userdata
+if [ ! -f /external_sd/userdata.img ]; then
+	touch /external_sd/userdata.img
+	dd if=/dev/zero of=/external_sd/userdata.img bs=512 count=3906250
+fi
 busybox dd if=/dev/zero of=/dev/block/platform/msm_sdcc.1/by-name/misc conv=notrunc bs=1 count=32 seek=64
-# Tell us that it worked!
+# Deneuter sim requirement from roms.
 rm -rf /system/app/CloudSetupWizard.apk
 rm -rf /system/app/ChargingWallpaperPickerbin.apk
 rm -rf /system/app/LGVZWSetupWizard_JB.apk
+sync
+umount /system
