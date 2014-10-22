5adebb6c86616177623b28821c0058d28824c227
lgrootnoob <lgrootnoob@github.com>
2014-10-22 09:42:50 -0500

    test twrp fix

--- recovery/postrecoveryboot.sh
+++ recovery/postrecoveryboot.sh
@@ -1,9 +1,9 @@
 #!/sbin/sh
-umount /system
+#umount /system
 #umount /external_sd
-busybox mount -o rw /dev/block/mmcblk0p14 /system
+#busybox mount -o rw /dev/block/mmcblk0p14 /system
 #busybox mount -o rw /dev/block/platform/msm_sdcc.3/by-num/p1 /external_sd
-echo "interactive" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
+#echo "interactive" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
 #mke2fs -t ext4 /dev/block/platform/msm_sdcc.1/by-name/userdata
 #if [ ! -f /external_sd/userdata.img ]; then
 #	dd if=/dev/zero of=/external_sd/userdata.img bs=512 count=3906250
@@ -11,8 +11,8 @@ echo "interactive" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
 #fi
 busybox dd if=/dev/zero of=/dev/block/platform/msm_sdcc.1/by-name/misc conv=notrunc bs=1 count=32 seek=64
 # Deneuter sim requirement from roms.
-rm -rf /system/app/CloudSetupWizard.apk
-rm -rf /system/app/ChargingWallpaperPickerbin.apk
-rm -rf /system/app/LGVZWSetupWizard_JB.apk
-sync
-umount /system
+#rm -rf /system/app/CloudSetupWizard.apk
+#rm -rf /system/app/ChargingWallpaperPickerbin.apk
+#rm -rf /system/app/LGVZWSetupWizard_JB.apk
+#sync
+#umount /system
